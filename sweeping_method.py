765016282d16f0fb81bebce9b3f28241d4388b94
aporo4000 <hiroshi4000@gmail.com>
2019-03-24 04:28:37 +0900

    update

--- sweeping_method.py
+++ sweeping_method.py
@@ -1,31 +1,30 @@
 # 掃き出し法
 import numpy as np
-
-#x = np.array([[2,2,3,10],[8,6,4,12],[11,8,6,14]])
-#x[0] = x[0]/x[0][0]
-#x[1] = x[1] - x[0]*x[1][0]
-#x[2] = x[2] - x[0]*x[2][0]
-#x[1] = x[1]/x[1][1]
-#x[2] = x[2] - x[1]*x[2][1]
-#x[2] = x[2]/x[2][2]
-
-#x_3 = x[2][2]
-#x_2 = x[1][3] - x[1][2]*x_3
-#x_1 = x[0][3] - (x[0][1]*x_2+x[0][2]*x_3)
+import sys
 
 
-# n次元
-#x = np.array([[2,2,3,10,11],[8,6,4,12,22],[11,8,6,14,44],[2,2,3,10,55]])
-x = np.random.rand(3,4)
+class SimultaneousEquation:
+    """連立方程式"""
 
-for i in range(len(x)):
-    if np.all(x[i][:i]>0):
-        for v in range(len(x[i][:i])):
-            x[i] = x[i] - x[v]*x[i][v]
-    if x[i][i] != 1:
-        x[i] = x[i]/x[i][i]
+    def __init__(self, input_data):
+        self._matrix = input_data
 
-# -0が出力をプラスの符号に変換
-x = x+0.
-print(x)
+    def sweeping_method(self):
+        """掃き出し法"""
+        for i in range(len(self._matrix)):
+            if np.all(self._matrix[i][:i]>0):
+                for v in range(len(self._matrix[i][:i])):
+                    self._matrix[i] = self._matrix[i] - self._matrix[v]*self._matrix[i][v]
+            if self._matrix[i][i] != 1:
+                self._matrix[i] = self._matrix[i]/self._matrix[i][i]
+        return self._matrix       
 
+
+if __name__=='__main__':
+    args = sys.argv
+    x = np.random.rand(3,4)
+    # x = args[1] 
+    simultaneous_equation = SimultaneousEquation(x)
+    x = simultaneous_equation.sweeping_method() 
+    print(x+0.)  # -0が出力をプラスの符号に変換
+    
